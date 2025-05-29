18e80eee2ff5a8e64a744105033f0d9cf5299e93
zokishmael <59413896+zokishmael@users.noreply.github.com>
2025-05-29 11:03:49 +0900

    Add files via upload

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+const config = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        background: 'rgb(var(--background))',
+        foreground: 'rgb(var(--foreground))',
+      }
+    }
+  }
+};
